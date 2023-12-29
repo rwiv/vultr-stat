692a285de6725b8689ba4732049111540cd9a4e4
rwiv <zc798546@gmail.com>
2023-12-29 23:07:24 +0900

    feat: add types

--- pkg/client/type_account.go
+++ pkg/client/type_account.go
@@ -13,3 +13,28 @@ type Account struct {
 	LastPaymentDate   string   `json:"last_payment_date"`
 	LastPaymentAmount float64  `json:"last_payment_amount"`
 }
+
+//export interface Account {
+//	balance: number; // 여분 잔액
+//	pending_charges: number; // 보류 중인 금액
+//	name: string; // 유저 이름(본명)
+//	email: string;
+//	acls: string[]; // 접근 가능 권한들. billing, firewall...
+//	last_payment_date: string; // 최근 결제일, ISO 포맷
+//	last_payment_amount: number;
+//}
+
+// billing/history
+//export interface BillingHistoryResponse {
+//	billing_history: BillingHistory[];
+//	meta: Meta;
+//}
+//
+//export interface BillingHistory {
+//	id: number
+//	date: string; // 결제일. ISO 포맷
+//	type: string; // 결제 타입. invoice, payment...
+//	description: string; // 세부 정보
+//	amount: number; // 금액, 달러 단위. 음수면 충전되었다는 것.
+//	balance: number; // 결제 후 잔액
+//}
--- pkg/client/type_service.go
+++ pkg/client/type_service.go
@@ -1,15 +1,5 @@
 package client
 
-type Meta struct {
-	Total int32 `json:"total"`
-	Links Links `json:"links"`
-}
-
-type Links struct {
-	Next string `json:"next"`
-	Prev string `json:"prev"`
-}
-
 type OsResponse struct {
 	Os   []*OS `json:"os"`
 	Meta *Meta `json:"meta"`
@@ -17,7 +7,40 @@ type OsResponse struct {
 
 type OS struct {
 	Id     int64  `json:"id"`
-	Name   string `json:"name"`
-	Arch   string `json:"arch"`
-	Family string `json:"family"`
+	Name   string `json:"name"`   // 풀네임
+	Arch   string `json:"arch"`   // x32, x64...
+	Family string `json:"family"` // windows, linux...
 }
+
+//export interface PlanResponse {
+//	plans: Plan[];
+//	meta: Meta;
+//}
+//
+//// /plans
+//export interface Plan {
+//	id: string; // 줄임말 형태
+//	vcpu_count: number; // cpu 코어 개수
+//	ram: number; // 램 용량, MB 단위
+//	disk: number; // 디스크 용량, GB 단위
+//	disk_count: number; // 디스크 개수
+//	bandwidth: number; // 대역폭, MB 단위
+//	monthly_cost: number; // 월별 가격, 달러 단위
+//	type: string;
+//	locations: any[];
+//}
+//
+
+//type RegionResponse struct {
+//	Regions []*Region `json:"regions"`
+//	Meta *Meta `json:"meta"`
+//}
+
+//// /regions
+//export interface Region {
+//	id: string; // 줄임말 형태
+//	city: string; // 도시명
+//	country: string; // 나라명 줄임말
+//	continent: string; // 대륙명
+//	options: any[];
+//}
